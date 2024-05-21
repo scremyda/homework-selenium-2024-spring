b9a131cdba31a391711e72b15d8c85a435669f57
Belozerovmsk <belozerov.msk@gmail.com>
2024-05-21 10:03:47 +0300

    <3

--- hw/code/test_settings.py
+++ hw/code/test_settings.py
@@ -6,12 +6,12 @@ from base import BaseCase
 class TestSettings(BaseCase):
     INPUT_ADD_ACCOUNT_VK_ID = "123456789"
     
-    def test_check_access_redirect(self, settings_page: SettingsPage):
+    def test_check_access_redirect(self, settings_page):
         settings_page.open_access_rights()
 
         assert self.driver.current_url == settings_page.locators.EXPECTED_ACCESS_URL
 
-    def test_check_access_additional_accounts_redirect(self, settings_page: SettingsPage):
+    def test_check_access_additional_accounts_redirect(self, settings_page):
         settings_page.open_access_rights()
 
         settings_page.open_access_rights_details()
@@ -19,14 +19,14 @@ class TestSettings(BaseCase):
         settings_page.switch_to_opened_window()
         assert self.driver.current_url == settings_page.locators.EXPECTED_ACCESS_DETAILS_URL
 
-    def test_check_access_add_account(self, settings_page: SettingsPage):
+    def test_check_access_add_account(self, settings_page):
         settings_page.open_access_rights()
 
         settings_page.open_access_rights_add_account()
 
         assert settings_page.check_opened_add_account_window()
 
-    def test_check_access_add_account_close(self, settings_page: SettingsPage):
+    def test_check_access_add_account_close(self, settings_page):
         settings_page.open_access_rights()
 
         settings_page.open_access_rights_add_account()
@@ -35,7 +35,7 @@ class TestSettings(BaseCase):
 
         assert settings_page.check_opened_add_account_window()
 
-    def test_check_access_add_account_id(self, settings_page: SettingsPage):
+    def test_check_access_add_account_id(self, settings_page):
         settings_page.open_access_rights()
 
         settings_page.open_access_rights_add_account()
@@ -46,7 +46,7 @@ class TestSettings(BaseCase):
 
         assert input_value == self.INPUT_ADD_ACCOUNT_VK_ID
 
-    def test_check_access_add_account_id_empty_input(self, settings_page: SettingsPage):
+    def test_check_access_add_account_id_empty_input(self, settings_page):
         settings_page.open_access_rights()
 
         settings_page.open_access_rights_add_account()
@@ -55,21 +55,21 @@ class TestSettings(BaseCase):
 
         assert settings_page.get_add_account_save_error()
 
-    def test_check_access_common_redirect(self, settings_page: SettingsPage):
+    def test_check_access_common_redirect(self, settings_page):
         settings_page.open_access_rights()
 
         settings_page.open_common()
 
         assert self.driver.current_url == settings_page.locators.EXPECTED_COMMON_URL
 
-    def test_common_user_language_list(self, settings_page: SettingsPage):
+    def test_common_user_language_list(self, settings_page):
         settings_page.scroll_to_about_access()
 
         settings_page.open_user_language_list()
 
         assert settings_page.get_user_language_list
 
-    def test_common_redirect_help_api(self, settings_page: SettingsPage):
+    def test_common_redirect_help_api(self, settings_page):
         settings_page.scroll_to_about_access()
 
         settings_page.open_more_about_access()
@@ -78,14 +78,14 @@ class TestSettings(BaseCase):
 
         assert self.driver.current_url == settings_page.locators.EXPECTED_COMMON_ABOUT_ACCESS_URL
 
-    def test_notifications_redirect(self, settings_page: SettingsPage):
+    def test_notifications_redirect(self, settings_page):
         settings_page.open_access_rights()
 
         settings_page.open_notifications()
 
         assert self.driver.current_url == settings_page.locators.EXPECTED_NOTIFICATIONS_URL
 
-    def test_notifications_connect_tg_redirect(self, settings_page: SettingsPage):
+    def test_notifications_connect_tg_redirect(self, settings_page):
         settings_page.open_notifications()
 
         settings_page.open_connect_tg()
@@ -94,12 +94,12 @@ class TestSettings(BaseCase):
 
         assert settings_page.locators.EXPECTED_NOTIFICATIONS_CONNECT_TG_URL in self.driver.current_url
 
-    def test_history_of_changes_redirect(self, settings_page: SettingsPage):
+    def test_history_of_changes_redirect(self, settings_page):
         settings_page.open_history_of_changes()
 
         assert self.driver.current_url == settings_page.locators.EXPECTED_CHANGED_HISTORY_URL
 
-    def test_history_of_changes_filter(self, settings_page: SettingsPage):
+    def test_history_of_changes_filter(self, settings_page):
         settings_page.open_history_of_changes()
 
         settings_page.open_changed_history_filter()
